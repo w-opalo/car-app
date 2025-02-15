d7f0d4c82e05a032ebed64e8129fc9c9160aece7
Willice Opalo <williceopalo11@gmail.com>
2025-02-15 17:52:30 +0300

    layout setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcomee');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/jobs/{id}', function ($id) {
+//     return "hello $id";
+// });
+// Route::get('/jobs/{id}/user/{id}', function ($id, $id) {
+//     return "hello $id";
+// });
+
+Route::get('/', [HomeController::class, 'index']);
