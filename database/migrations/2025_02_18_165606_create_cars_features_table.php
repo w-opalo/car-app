a4d498d0391496205fa22be2e140f5e64fad1dfb
Willice Opalo <williceopalo11@gmail.com>
2025-02-18 22:10:14 +0300

    databage migrations made

---
+++ database/migrations/2025_02_18_165606_create_cars_features_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars_features', function (Blueprint $table) {
+            $table->foreignId('car_id')->constrained('cars')->primary();
+            $table->boolean('abs')->default(0);
+            $table->boolean('air_conditioning')->default(0);
+            $table->boolean('power_windows')->default(0);
+            $table->boolean('power_door_locks')->default(0);
+            $table->boolean('cruise_control')->default(0);
+            $table->boolean('bluetooth_connectivity')->default(0);
+            $table->boolean('gps_navigation')->default(0);
+            $table->boolean('heated_seats')->default(0);
+            $table->boolean('climate_control')->default(0);
+            $table->boolean('rear_parking_sensors')->default(0);
+            $table->boolean('leather_seats')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars_features');
+    }
+};
