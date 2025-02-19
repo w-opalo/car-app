40952ab31c14c1e9fed6ac81750e4c61442fd176
Willice Opalo <williceopalo11@gmail.com>
2025-02-19 11:43:47 +0300

    Models creation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
@@ -29,5 +30,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/search', [CarController::class, 'search']);
+Route::get('/car', CarController::class);
 Route::get('/signup', [SignupController::class, 'create']);
 Route::get('/login', [LoginController::class, 'login']);
